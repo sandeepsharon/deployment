f3ebe81c328505b3700c7968c8b193c539b1cfeb
sraeik <32627609+sandeepsharon@users.noreply.github.com>
2020-01-28 01:00:09 +0530

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+RED='\033[0;31m'
+NC='\033[0m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+WHITE='\033[1;37m'
+domain_name=fibidev.ntu.edu.sg
+files=/root/list
+list=`ls /root/list`
+webapps=/opt/tomcat/webapps
+backup_location=/opt/backup
+count=`echo $list | wc -w`
+current_date=`echo "$(date +"%d-%m-%Y")"`
+#current_time=`echo "$(date +%H:%M:%S:%N)"`
+time=$(date +"%d_%m_%Y-%I:%M:%p")
+tomcat_shutdown() {
+ process=`pstree -apu | grep "[D]java.util" | awk '{print $2}' | cut -c8-`
+ for j in $process; do kill -9 $j; done
+}
+if [ $count -ne 0 ]; then
+ echo -e "You are about to perform deployment in ${RED}$domain_name${NC}"
+ echo "Please wait ......"
+ for i in $list;
+  do
+   if [[ "$i" =~ ^(fibi_ntu|fibi4_ntu.war)$ ]]; then
+    sh /opt/tomcat/bin/shutdown.sh > /dev/null 2>&1
+    
+    tomcat_shutdown
+    mkdir -p $backup_location/$current_date
+    rm -rf $backup_location/$current_date/$i    
+    mv $webapps/$i $backup_location/$current_date/
+    rm -rf $webapps/$i
+    cp -R $files/$i $webapps
+    sh /opt/tomcat/bin/startup.sh > /dev/null 2>&1
+    echo -e "Backup of $i is taken"
+    echo -e "${GREEN}Deployment of $i done${NC}"
+   
+   else
+    echo -e "${RED}Illegal file $i cannot be deployed${NC}"
+   fi
+ sleep 1
+ done
+while true; do
+    sleep 2
+    read -p "Do you wish to see tomcat logs? [y/n]" yn
+    case $yn in
+        [Yy]* ) tail -f /opt/tomcat/logs/catalina.out;;
+        [Nn]* ) exit;;
+        * ) echo -e "${RED}Please answer y or n${NC}";;
+    esac
+    done
+else
+ echo -e "${RED}File Not Found!!!${NC}"
+fi
