a8eed87405c62898cb8183a3e2069373e81aa61f
sraeik <32627609+sandeepsharon@users.noreply.github.com>
2020-09-29 13:35:42 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,12 @@ NC='\033[0m'
 GREEN='\033[0;32m'
 YELLOW='\033[0;33m'
 WHITE='\033[1;37m'
+suffix=.war
 flag=0
+war1=fibi-ntu.war
+war2=fibi4_ntu.war
+war3=fibi_ntu
+war4=fibi-orcid.war
 domain_name=fibidev.ntu.edu.sg
 files=/home/arjun.chand/today/boomer
 list=`ls $files`
@@ -18,23 +23,30 @@ tomcat_shutdown() {
  process=`pstree -apu | grep "[D]java.util" | sed 's/[^0-9]*//g'`
  for j in $process; do kill -9 $j; done
 }
+war_cut() {
+ del=`echo "$i" | sed -e "s/$suffix$//"`
+ rm -rf $webapps/$del
+ }
 if [ $count -ne 0 ]; then
  echo -e "You are about to perform deployment in ${RED}$domain_name${NC}"
  echo "Please wait ......"
  for i in $list;
   do
-   if [[ "$i" =~ ^(fibi_ntu|fibi4_ntu.war|fibi-ntu.war|testfibi)$ ]]; then
+   if [[ "$i" =~ ^($war1|$war2|$war3|$war4)$ ]]; then
     systemctl stop tomcat > /dev/null 2>&1
     sleep 5
     tomcat_shutdown
     mkdir -p $backup_location/$current_date
     #rm -rf $backup_location/$current_date/$i
     mv $webapps/$i $backup_location/$current_date/${i}_$time
-    if [ $i == "fibi4_ntu.war" ]; then
-     rm -rf $webapps/fibi4_ntu
+    if [ $i == $war2 ]; then
+     war_cut
     fi
-    if [ $i == "fibi-ntu.war" ]; then
-     rm -rf $webapps/fibi-ntu
+    if [ $i == $war1 ]; then
+     war_cut
+    fi
+    if [ $i == $war4 ]; then
+     war_cut
     fi
 
     cp -R $files/$i $webapps
