96a29c924a4cf1504a3c0f0d4ebeed67ccc7b7e6
sraeik <32627609+sandeepsharon@users.noreply.github.com>
2020-09-30 03:38:44 +0530

    Update deploy3.sh

--- deploy3.sh
+++ deploy3.sh
@@ -74,6 +74,7 @@ if [ $count -ne 0 ]; then
  done
   if [ $flag -gt 0 ]; then
    systemctl start tomcat > /dev/null 2>&1
+   echo "Please wait while Tomcat starts ..........."
    ping
    echo -e "${GREEN}Tomcat server is up now${NC}"
   fi
